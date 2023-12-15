e8af5c98c866b24dc9ca062c9fb2fc95196c3538
Leon <71514953+LeonVilovic@users.noreply.github.com>
2023-12-15 11:10:33 +0100

    Update question5_answer.sql

--- question5_answer.sql
+++ question5_answer.sql
@@ -2,7 +2,7 @@
 
 EXEC('EXEC sp_isThisACoolDate ?', '2022-11-22') AT coolDates
 
---A) procedure sp_isThisACoolDate will be executed with parametar '2022-11-22'
+--A) procedure sp_isThisACoolDate will be executed with parametar '2022-11-22' with result set definition equal to 'coolDates'
 --B) code will try to execute procedure sp_isThisACoolDate with parameter ? which will raise an error
 --C) procedure sp_isThisACoolDate will be executed with parametar '2022-11-22' at linked server with alias 'coolDates'
 --D) procedure sp_isThisACoolDate will be executed with no parametars
