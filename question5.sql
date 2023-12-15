5011b997da1433170363e16df2805d9aa1e57f0b
Leon <71514953+LeonVilovic@users.noreply.github.com>
2023-12-15 10:55:26 +0100

    Create question5.sql

---
+++ question5.sql
@@ -0,0 +1,8 @@
+-- What will be the result of executing the following SQL code?
+
+EXEC('EXEC sp_isThisACoolDate ?', '2022-11-22') AT coolDates
+
+--A) procedure sp_isThisACoolDate will be executed with parametar '2022-11-22'
+--B) code will try to execute procedure sp_isThisACoolDate with parameter ? which will raise an error
+--C) procedure sp_isThisACoolDate will be executed with parametar '2022-11-22' at linked server with alias 'coolDates'
+--D) procedure sp_isThisACoolDate will be executed with no parametars
