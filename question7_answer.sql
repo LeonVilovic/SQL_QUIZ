22dde68a1c03ec8b55c27ef5c8c9e3512fe03395
Leon <71514953+LeonVilovic@users.noreply.github.com>
2023-12-20 11:58:17 +0100

    Update question7_answer.sql

--- question7_answer.sql
+++ question7_answer.sql
@@ -24,10 +24,10 @@ FROM #Requests a
 GROUP BY EmployeeID
 HAVING count(EmployeeID) > 1
 
---A) Result set: R: 1 D: 2023-01-10 R: 2 D: 2023-02-11  
---B) Result set: R: 1 D: 2023-01-10 R: 2 D: 2023-02-11 , R: 3 D: 2024-07-05 R: 4 D: 2020-05-02  
---C) Result set: R: 5 D: 2021-02-24 , R: 6 D: 2023-03-15 , R: 7 D: 2022-04-01   
---D) Result set: R: 3 D: 2024-07-05 R: 4 D: 2020-05-02  
+--A) Result set: R: 1 D: 2023-01-10 R: 2 D: 2023-02-11
+--B) Result set: R: 1 D: 2023-01-10 R: 2 D: 2023-02-11 , R: 3 D: 2024-07-05 R: 4 D: 2020-05-02
+--C) Result set: R: 5 D: 2021-02-24 , R: 6 D: 2023-03-15 , R: 7 D: 2022-04-01
+--D) Result set: R: 3 D: 2024-07-05 R: 4 D: 2020-05-02
 
 
 
