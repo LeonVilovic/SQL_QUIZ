0afadc960afa6b33272731e1c9dc7548fa1bd1a5
Leon <71514953+LeonVilovic@users.noreply.github.com>
2023-12-22 14:24:40 +0100

    Update question4.sql

--- question4.sql
+++ question4.sql
@@ -1,40 +1,26 @@
 -- What will be the result of executing the following SQL code?
 
-CREATE TABLE #Employees (
-    EmployeeID INT PRIMARY KEY,
-    FirstName NVARCHAR(50),
-    LastName NVARCHAR(50),
-    Salary DECIMAL(10, 2),
-    DepartmentID INT
+CREATE TABLE #Temp (
+    Number INT
 );
 
-CREATE TABLE #Departments (
-    DepartmentID INT PRIMARY KEY,
-    DepartmentName NVARCHAR(50)
-);
-
-INSERT INTO #Departments (DepartmentID, DepartmentName)
+INSERT INTO #Temp
 VALUES
-    (1, 'IT'),
-    (2, 'HR'),
-    (3, 'Finance');
+    (1),(2),(3),(4);
 
-INSERT INTO #Employees (EmployeeID, FirstName, LastName, Salary, DepartmentID)
-VALUES
-    (1, 'John', 'Doe', 60000.00, 1),
-    (2, 'Jane', 'Smith', 55000.00, 2),
-    (3, 'Michael', 'Johnson', 70000.00, 2),
-    (4, 'Emily', 'Williams', 62000.00, 1);
+SELECT TOP 1 * FROM #Temp
+ORDER BY NEWID()
 
-SELECT 
-    E.FirstName,
-    D.DepartmentName,
-    Salary * 1.1 AS IncreasedSalary
-FROM #Employees E
-JOIN #Departments D ON E.DepartmentID = D.DepartmentID
-WHERE Salary > (SELECT AVG(Salary) FROM #Employees WHERE DepartmentID = E.DepartmentID);
 
---A) The code will raise an error 
---B) Result set: Emily IT 68200.000, Michael HR 77000.000
---C) Empty result set.
---D) Result set: Michael HR 77000.000
+--A) Result set: 1
+--B) Result set: 4
+--C) The code will return a random row from table #Temp as a result set 
+--D) The code will raise an error
+
+
+
+
+-- CORRECT ANSWER : C) The code will return a random row from table #Temp as a result set 
+-- NEWID is a function used to generete globally unique identifiers (GUID) which are strings that, for example, 
+-- look like this: 1B4E92B5-4353-4968-A14A-15AD3D5E1D2E  
+-- But you can also use NEWID in the ORDER BY clause to query random data, like in the example above.
