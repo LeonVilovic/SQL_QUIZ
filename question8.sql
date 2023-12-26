0ec7f3ad6871ed2f2aeda614e4589b312b5898a8
Leon <71514953+LeonVilovic@users.noreply.github.com>
2023-12-26 10:32:35 +0100

    Create question8.sql

---
+++ question8.sql
@@ -0,0 +1,26 @@
+--Sometimes code you are working on can get messy!
+--Is there a keyboard shortcut that will help you find  
+--where bracket in line 10 is closed?
+
+CREATE TABLE #Temp (Number INT);
+INSERT INTO #Temp VALUES (1),(2),(3),(4);
+
+BEGIN/* [( }}(*]]})  ))}]*/  BEGIN/*))]}])))))*/(
+/* [[ (}(])(())(}]]*/(/*))]}]]*/(/*))]}]) )] ))*/
+/*Where do I close?>>>*/(/*<<<Where do I close?*/
+((/* (( }})]*/ (/*)[[ (})(*/ (/* [ [}}]]*/(/*(]*/
+/* [( }(]]*/( SELECT * FROM #Temp/* [(])))(()](*/ 
+/* [[))(]}] ]}(]  ]})((])}(]}))  )]]}  ))))))]]*/
+) /* ([)(}(](}](}(}]( }] ]}] ])  ))))]}((}((}(]*/
+)/* [[]]*/)/* (]*/ )/*((]*/)/* [[ ))) )))(())]]*/
+/* [[ }}(]]}]]}]]*/)/*))]}]]*/)/* [[ }(]]}]]}]]*/
+/* [[ }(]}] ]}(]  ]}]]*((((}]]}]]*}))  )]]}  ]]*/
+/* ( }}(]]}]]}]]*/)/*))]}]]*/)/* [[ }}(]]}]}]]*/)
+print('')/*))]}];*/;/* ;[[[ }]))]]  ]]]]  ]))]]*/
+/* [[ }(]}] ]}(]  ]})]})]})]}))  )]]}  )]}))))]*/
+END/*)[[ )}))*/  END/* [[ }}])))]]}]])))])))]]]*/
+
+--A) YES keyboard shortcut: Ctrl + ]
+--B) YES keyboard shortcut: Ctrl + K, Ctrl + S
+--C) YES keyboard shortcut: Ctrl + End
+--D) NO there is no such shortcut
